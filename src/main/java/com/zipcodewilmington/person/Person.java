0e5f8e7704bdf637d0f6a87d9997024561d702ba
Zaina Cruz-King <zainacruz-king@zipcoders-MacBook-Pro.local>
2019-02-24 18:55:51 -0500

    ehatgf

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -4,32 +4,39 @@ package com.zipcodewilmington.person;
  * Created by leon on 2/12/18.
  */
 public class Person {
-    private String name;
-    private int age;
+    private String name = "";
+    private int age = Integer.MAX_VALUE;
 
     public Person() {
+
     }
 
     public Person(int age) {
-    }
+        this.age = age;
+     }
 
     public Person(String name) {
+    this.name = name;
     }
 
     public Person(String name, int age) {
+        this.age = age;
+        this.name = name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+        return this.age;
     }
 }
